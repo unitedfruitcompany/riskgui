d1cd88aa8f4330caa954ddad0c7027dea491cc01
cb745 <cb745@cornell.edu>
2022-03-30 15:51:22 -0400

    map

--- bin/main.ml
+++ bin/main.ml
@@ -12,10 +12,11 @@ let p1 : Player.player =
 let _ = print_endline (Player.string_of_player p1)
 
 let main () =
+  let map = W.image "risk_board.png" in 
   let input = W.text_input ~max_size:200 ~prompt:"Enter your name" () in
   let label = W.label ~size:10 (Player.string_of_player p1) in
   let layout = L.tower [L.resident ~w:1000 input;
-                       L.resident ~w:1000 ~h:400 label] in
+                       L.resident ~w:1000 ~h:400 label; L.resident ~w:100 ~h:100 map] in
   let action ti l _ =
     let text = W.get_text ti in
     W.set_text l ("Hello " ^ text ^ "!") in
